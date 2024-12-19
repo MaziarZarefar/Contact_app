89337ad925703c7dfe340b55e189fb548d337106
Maziar Zarefar <maziarzrfr@gmail.com>
2024-12-19 15:45:23 +0330

    contact

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css"
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Contact App</h1>
+        <p>
+            <a href="https://botostart.ir">
+                Botostart
+            </a> | React.js Full Course
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
